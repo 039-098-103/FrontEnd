d7db68e37efc83e93b85475fc47c589f417b75f2
Apisara Ngakor <apisara.99@mail.kmutt.ac.th>
2021-11-17 22:05:33 +0700

    UPDATE Customer2

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import './assets/tailwind.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
-import MenuIcon from 'vue-material-design-icons/Menu.vue';
 import NavAdmin from '@/components/NavAdmin.vue'
 import AddStaff from '@/components/AddStaffButtom.vue'
 import EditProfile from '@/components/EditProfile.vue'
@@ -17,7 +16,6 @@ const app = createApp(App).use(router)
 app.component('navAdmin', NavAdmin)
 app.component('addStaff', AddStaff)
 app.component('edit', EditProfile)
-app.component('md-icon', MenuIcon)
 app.component('navbar', CusNav);
 app.component('navProduct', NavProduct)
 app.component('popup', PopUp)
